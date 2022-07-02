a6199d20e00b4995b6b09a09b1381a0e16f053c1
Antonio Castilho <castilhomec@gmail.com>
2022-07-02 11:21:39 -0300

    first commit

---
+++ helloword.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Variavel String
+  String? nome = "Antonio";
+  String? sobrenome = "Castilho";
+  String? cor = "Azul";
+  String? tamanho;
+  print(nome + " " + sobrenome);
+  print(cor);
+  String? resultado = cor ?? "Sem Cor";
+  String? resulta = tamanho ?? "Sem tamanho";
+  print(resultado);
+  // -------------------------------------------------------------------
+  int valor = 10;
+  print(valor);
+  double valorquebrado = 5.5;
+  print(valorquebrado);
+  //--------------------------------------------------------------------
+  
+}
